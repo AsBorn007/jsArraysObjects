a5759b73b5b31e74cb0a6fa3f36c0d2de931883e
AsBorn007 <pk175040@gmail.com>
2025-01-09 20:30:57 +0530

    find arrays item

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 // array is a collection of diffrent datatypes or elements
 let arraysData = [25,"PRavven" ,true,]
 let showArrays =  document.getElementById("showData");
+let showDataIndex =  document.getElementById("showDataIndex");
 showArrays.innerHTML = arraysData;
+
+
+let findValueUsingIndex = arraysData[0];  //this give result that is String Praveen
+showArrays.innerHTML = findValueUsingIndex;
+
+let showDataIndexVal = arraysData[1];  //this give result that is String Praveen
+showDataIndex.innerHTML = showDataIndexVal;
