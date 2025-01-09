65af3f1352f164830257bfc91e98bfdc8656b98a
AsBorn007 <pk175040@gmail.com>
2025-01-09 20:15:59 +0530

    files created

---
+++ main.js
@@ -0,0 +1,4 @@
+// array is a collection of diffrent datatypes or elements
+let arraysData = [25,"PRavven" ,true,]
+let showArrays =  document.getElementById("showData");
+showArrays.innerHTML = arraysData;
